a5679bbca104ace9fa2b80910fcf38973537262a
adam shamis <shuka.fred@gmail.com>
2021-05-09 17:23:26 +0300

    Fix Login_request_handler

--- TriviaProject/TriviaProject/LoginRequestHandler.cpp
+++ TriviaProject/TriviaProject/LoginRequestHandler.cpp
@@ -10,51 +10,14 @@ bool LoginRequestHandler::isRequestRelevant(RequestInfo info)
 
 RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
 {
-    bool success = true;
     RequestResult myResult;
     if (info.requestId == LOGIN_CODE)
     {
-        LoginResponse response;
-        LoginRequest myRequest = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
-        try
-        {
-          m_loginManager->login(myRequest.username, myRequest.password);
-        }
-        catch (...)
-        {
-            success = false;
-            response.status = ERR_CODE;
-            myResult.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
-            myResult.newhandler = nullptr;
-        }
-        if (success)
-        {
-            myResult.newhandler = new MenuRequestHandler;
-            response.status = SUCCESS;
-            myResult.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
-        }
+        myResult = login(info);
     }
     else if(info.requestId == SIGNUP_CODE)
     {
-        SignUpResponse response;
-        SignupRequest myRequest = JsonRequestPacketDeserializer::deserializeSignupRequest(info.buffer);
-        try
-        {
-           m_loginManager->signup(myRequest.username, myRequest.password , myRequest.email);
-        }
-        catch (...)
-        {
-            success = false;
-            response.status = ERR_CODE;
-            myResult.response = JsonResponsePacketSerializer::serializeSignUpResponse(response);
-            myResult.newhandler = nullptr;
-        }
-        if (success)
-        {
-            myResult.newhandler = new MenuRequestHandler;
-            response.status = SUCCESS;
-            myResult.response = JsonResponsePacketSerializer::serializeSignUpResponse(response);
-        }
+        myResult = signup(info);
     }
     else if(info.requestId == ERR_CODE)
     {
@@ -62,6 +25,58 @@ RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
         response.message = "Bad request...";
         myResult.response = JsonResponsePacketSerializer::serializeErrorResponse(response);
         myResult.newhandler = nullptr;
+    }
+    return myResult;
+}
+
+RequestResult LoginRequestHandler::login(RequestInfo info)
+{
+    RequestResult myResult;
+    bool success = true;
+    LoginResponse response;
+    LoginRequest myRequest = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+    try
+    {
+        m_loginManager->login(myRequest.username, myRequest.password);
+    }
+    catch (...)
+    {
+        success = false;
+        response.status = ERR_CODE;
+        myResult.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
+        myResult.newhandler = nullptr;
+    }
+    if (success)
+    {
+        myResult.newhandler = new MenuRequestHandler;
+        response.status = SUCCESS;
+        myResult.response = JsonResponsePacketSerializer::serializeLoginResponse(response);
+    }
+    return myResult;
+}
+
+RequestResult LoginRequestHandler::signup(RequestInfo info)
+{
+    RequestResult myResult;
+    bool success = true;
+    SignUpResponse response;
+    SignupRequest myRequest = JsonRequestPacketDeserializer::deserializeSignupRequest(info.buffer);
+    try
+    {
+        m_loginManager->signup(myRequest.username, myRequest.password, myRequest.email);
+    }
+    catch (...)
+    {
+        success = false;
+        response.status = ERR_CODE;
+        myResult.response = JsonResponsePacketSerializer::serializeSignUpResponse(response);
+        myResult.newhandler = nullptr;
+    }
+    if (success)
+    {
+        myResult.newhandler = new MenuRequestHandler;
+        response.status = SUCCESS;
+        myResult.response = JsonResponsePacketSerializer::serializeSignUpResponse(response);
     }
     return myResult;
 }
--- TriviaProject/TriviaProject/LoginRequestHandler.h
+++ TriviaProject/TriviaProject/LoginRequestHandler.h
@@ -7,6 +7,8 @@ class LoginRequestHandler : public IRequestHandler
 public:
 	bool isRequestRelevant(RequestInfo info) override;
 	RequestResult handleRequest(RequestInfo info) override;
+	RequestResult login(RequestInfo info);
+	RequestResult signup(RequestInfo info);
 private:
 	static LoginManager* m_loginManager;
 };

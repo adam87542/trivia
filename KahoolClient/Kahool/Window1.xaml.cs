8148e2bc403fced7d3510c85eb62adeeeaed0753
Michael Klotsky <michael956244@gmail.com>
2021-05-22 16:19:13 +0300

    Done with login window and created a basis for signup #20

---
+++ KahoolClient/Kahool/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kahool
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+
+		public void EndRunning(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+	}
+}

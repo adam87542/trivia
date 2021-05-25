fa70253126ec58f32a09402ee4556258f7a64aa8
adam shamis <shuka.fred@gmail.com>
2021-05-25 23:10:32 +0300

    Added MenuWindow

---
+++ KahoolClient/Kahool/MenuWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kahool
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnCreateRoomClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnJoinRoomClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void EndRunning(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}

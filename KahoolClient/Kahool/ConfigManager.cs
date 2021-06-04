36c1e543b144663bcf06219ca78ed367de3ff4cd
Michael Klotsky <michael956244@gmail.com>
2021-06-04 16:58:52 +0300

    Done with config manager #26

---
+++ KahoolClient/Kahool/ConfigManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kahool
+{
+	class ConfigManager
+	{
+		private static string[] getConfigContent()
+		{
+			return System.IO.File.ReadAllLines("config.txt");
+		}
+
+		public static string getConfigIP()
+		{
+			return getConfigContent()[0].Split("-")[1];
+		}
+
+		public static string getConfigPort()
+		{
+			return getConfigContent()[1].Split("-")[1];
+		}
+
+		public static string getConfigEncrypt()
+		{
+			return getConfigContent()[2].Split("-")[1];
+		}
+	}
+}

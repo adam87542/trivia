1b865f36748029f736405ce419309abc2599e847
Sean Shuhman <7474846-SeanCa201@users.noreply.gitlab.com>
2021-06-26 21:20:07 +0300

    Added question page #41

---
+++ KahoolClient/Kahool/Question.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Kahool
+{
+    /// <summary>
+    /// Interaction logic for Question.xaml
+    /// </summary>
+    public partial class Question : Page
+    {
+        private int time; 
+        public Question(int time)
+        {
+            this.time = time;
+            InitializeComponent();
+
+            Thread timer = new Thread(TimeCounter);
+            timer.Start();
+        }
+
+        public void TimeCounter()
+        {
+            for (int i = time; i >= 0; i--)
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    Timer.Content = "Time: " + i;
+                });
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
